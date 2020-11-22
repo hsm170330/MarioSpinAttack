b435d1bb22a06338ba9df55730602437cce293ef
Hitarth Mistry <hitarthmistry42@gmail.com>
2020-11-21 21:11:38 -0600

    imported assets and polished up camera and movement

---
+++ Assets/Scripts/MarioMovement.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MarioMovement : MonoBehaviour
+{
+    public CharacterController mario;
+
+    public float speed = 20f;
+    public float gravity = -9.81f;
+    public float jumpHeight = 3f;
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public float turnSmoothTime = 0.1f;
+    public float turnSmoothVelocity;
+    public LayerMask groundMask;
+    Vector3 velocity;
+    bool isGrounded;
+    bool isAlive;
+
+    public Transform cam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isAlive = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        if (isAlive)
+        {
+            float horizontal = Input.GetAxisRaw("Horizontal");
+            float vertical = Input.GetAxisRaw("Vertical");
+
+            Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+            if (direction.magnitude >= 0.1f)
+            {
+                float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+                float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+                transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+                Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+                mario.Move(moveDir.normalized * speed * Time.deltaTime);
+            }
+            
+
+            if (Input.GetButtonDown("Jump") && isGrounded)
+            {
+                velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            }
+            velocity.y += gravity * Time.deltaTime;
+
+            mario.Move(velocity * Time.deltaTime);
+        }
+    }
+}
---
+++ Assets/Scripts/ThirdPersonCamera.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCamera : MonoBehaviour
+{
+    [SerializeField] private float distanceAway = 5;
+    [SerializeField] private float distanceUp = 2;
+    [SerializeField] private float smooth = 3;
+    [SerializeField] private Transform follow;
+    private Vector3 targetPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        follow = GameObject.FindWithTag("Player").transform;
+    }
+
+    void LateUpdate()
+    {
+        //setting the offset
+        targetPosition = follow.position + Vector3.up * distanceUp - follow.forward * distanceAway;
+
+        //making the transition from it's position to the new position
+        transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * smooth);
+
+        //camera looking the right way
+        transform.LookAt(follow);
+    }
+}
